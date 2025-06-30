8393781966ba11aa9a539fb5147c91d8e681eeae
devcambo <devcambo001@gmail.com>
2025-06-30 08:45:50 +0700

    set up prettier code format

--- fullstack-spring-react-jwt-template-v1/backend-api/src/main/java/com/devcambo/backendapi/BackendApiApplication.java
+++ fullstack-spring-react-jwt-template-v1/backend-api/src/main/java/com/devcambo/backendapi/BackendApiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BackendApiApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(BackendApiApplication.class, args);
+  }
 }
